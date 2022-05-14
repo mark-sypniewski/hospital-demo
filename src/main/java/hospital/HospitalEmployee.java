f7bbfca0f63ee69831cc54696b11fa87bf29b3b2
mark-sypniewski <msyp721@gmail.com>
2022-05-14 00:00:55 -0400

    Full Demo

--- src/main/java/hospital/HospitalEmployee.java
+++ src/main/java/hospital/HospitalEmployee.java
@@ -5,6 +5,14 @@ public abstract class HospitalEmployee {
     protected String employeeNumber;
     protected String name;
 
+    public String getEmployeeNumber() {
+        return employeeNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
     public HospitalEmployee(String employeeNumber, String name) {
         this.employeeNumber = employeeNumber;
         this.name = name;
--- src/main/java/hospital/Patient.java
+++ src/main/java/hospital/Patient.java
@@ -4,17 +4,26 @@ public class Patient {
     private int healthLevel;
     private int bloodLevel;
 
+
     public int getHealthLevel() {
         return healthLevel;
     }
+
     public int getBloodLevel() {
         return bloodLevel;
     }
+
 
     //default constructor for patient object
     public Patient() {
         healthLevel = 10; //default patient health level
         bloodLevel = 20; //default patient blood level
+    }
+
+    //overloaded constructor (constructors are very popular code blocks to overload)
+    public Patient(int healthLevel, int bloodLevel){
+        this.healthLevel = healthLevel;
+        this.bloodLevel = bloodLevel;
     }
 
     public void increaseHealthLevel(int healthIncreaseAmount) {
